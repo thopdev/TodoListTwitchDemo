d661c83399c230f41a1ed7b62491c5d41a89f49b
Thomas Hop <thomas.hhopp@gmail.com>
2020-12-28 19:22:55 +0100

    Add project files.

---
+++ Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todo.Models
+{
+    public class TodoItem
+    {
+        public string Name { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ Pages/TodoListComponent.razor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Todo.Models;
+
+namespace Todo.Pages
+{
+    public partial class TodoListComponent
+    {
+        public IList<TodoItem> TodoItems { get; } = new List<TodoItem>{new TodoItem{Name= "Add Css"}, new TodoItem{Name = "Add API"}, new TodoItem { Name = "Grab a drink" } };
+
+        public string NewTodoItemName { get; set; }
+
+        public void AddTodoItemToList()
+        {
+            TodoItems.Add(new TodoItem(){Name = NewTodoItemName});
+            NewTodoItemName = string.Empty;
+        }
+    }
+}
