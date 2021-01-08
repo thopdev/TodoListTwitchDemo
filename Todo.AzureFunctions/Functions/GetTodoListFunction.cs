0e6eb8dd37ea60644fc3b4ae00263e95233531ab
Thomas Hop <thomas.hhopp@gmail.com>
2021-01-08 20:28:39 +0100

    x

--- Todo.AzureFunctions/Functions/GetTodoListFunction.cs
+++ Todo.AzureFunctions/Functions/GetTodoListFunction.cs
@@ -62,42 +62,4 @@ namespace Todo.AzureFunctions.Functions
             return new OkObjectResult(_mapper.Map<IEnumerable<TodoItemDto>>(todoList));
         }
     }
-
-    public static class StaticWebAppsAuth
-    {
-        private class ClientPrincipal
-        {
-            public string IdentityProvider { get; set; }
-            public string UserId { get; set; }
-            public string UserDetails { get; set; }
-            public IEnumerable<string> UserRoles { get; set; }
-        }
-
-        public static ClaimsPrincipal Parse(HttpRequest req)
-        {
-            var principal = new ClientPrincipal();
-
-            if (req.Headers.TryGetValue("x-ms-client-principal", out var header))
-            {
-                var data = header[0];
-                var decoded = Convert.FromBase64String(data);
-                var json = Encoding.ASCII.GetString(decoded);
-                principal = JsonSerializer.Deserialize<ClientPrincipal>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-            }
-
-            principal.UserRoles = principal.UserRoles?.Except(new string[] { "anonymous" }, StringComparer.CurrentCultureIgnoreCase);
-
-            if (!principal.UserRoles?.Any() ?? true)
-            {
-                return new ClaimsPrincipal();
-            }
-
-            var identity = new ClaimsIdentity(principal.IdentityProvider);
-            identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, principal.UserId));
-            identity.AddClaim(new Claim(ClaimTypes.Name, principal.UserDetails));
-            identity.AddClaims(principal.UserRoles.Select(r => new Claim(ClaimTypes.Role, r)));
-
-            return new ClaimsPrincipal(identity);
-        }
-    }
 }
