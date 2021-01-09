3842c5c08e359400e5792501523728c4aa46db85
Thomas Hop <thomas.hhopp@gmail.com>
2021-01-09 16:28:11 +0100

    x

--- Todo/Program.cs
+++ Todo/Program.cs
@@ -18,7 +18,7 @@ namespace Todo
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:7071/api") });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress + "api") });
 
             builder.Services.AddServices();
 
