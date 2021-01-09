8af3ed112d0c3410b80f2c47b45a8626cf68f71f
Thomas Hop <thomas.hhopp@gmail.com>
2021-01-09 20:46:08 +0100

    x

--- Todo.AzureFunctions/Functions/GetTodoListFunction.cs
+++ Todo.AzureFunctions/Functions/GetTodoListFunction.cs
@@ -36,8 +36,6 @@ namespace Todo.AzureFunctions.Functions
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]
             HttpRequest req, ClaimsPrincipal claims)
         {
-            return new OkObjectResult("Hi");
-
             try
             {
                 var header = StaticWebAppsAuth.Parse(req);
@@ -86,7 +84,7 @@ namespace Todo.AzureFunctions.Functions
 
         public static ClaimsPrincipal Parse(HttpRequest req)
         {
-            var principal = new ClientPrincipal();
+            var principal = new ClaimsPrincipal();
 
             if (req.Headers.TryGetValue("x-ms-client-principal", out var header))
             {
@@ -95,20 +93,20 @@ namespace Todo.AzureFunctions.Functions
                 var json = Encoding.ASCII.GetString(decoded);
                 principal = JsonSerializer.Deserialize<ClientPrincipal>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
             }
-
-            principal.UserRoles = principal.UserRoles?.Except(new string[] { "anonymous" }, StringComparer.CurrentCultureIgnoreCase);
-
-            if (!principal.UserRoles?.Any() ?? true)
-            {
-                return new ClaimsPrincipal();
-            }
-
-            var identity = new ClaimsIdentity(principal.IdentityProvider);
-            identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, principal.UserId));
-            identity.AddClaim(new Claim(ClaimTypes.Name, principal.UserDetails));
-            identity.AddClaims(principal.UserRoles.Select(r => new Claim(ClaimTypes.Role, r)));
-
-            return new ClaimsPrincipal(identity);
+            return principal;
+            // principal.UserRoles = principal.UserRoles?.Except(new [] { "anonymous" }, StringComparer.CurrentCultureIgnoreCase);
+            //
+            // if (!principal.UserRoles?.Any() ?? true)
+            // {
+            //     return new ClaimsPrincipal();
+            // }
+            //
+            // var identity = new ClaimsIdentity(principal.IdentityProvider);
+            // identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, principal.UserId));
+            // identity.AddClaim(new Claim(ClaimTypes.Name, principal.UserDetails));
+            // identity.AddClaims(principal.UserRoles.Select(r => new Claim(ClaimTypes.Role, r)));
+            //
+            // return new ClaimsPrincipal(identity);
         }
     }
 }
